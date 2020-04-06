588b580cbe288901dde3d0fb00fb2b967c610dff
Nayil <n.arana1@outlook.com>
2020-04-06 18:01:20 -0500

    Included ggplot2 graphs in code

--- Code for Ethnicity vs. Perceived Age.R
+++ Code for Ethnicity vs. Perceived Age.R
@@ -1,3 +1,6 @@
+install.packages("ggplot2")
+library("ggplot2")
+theme_update(plot.title = element_text(hjust = 0.5))
 guesses = read.csv("2019_project_data.csv", head=T)
 head(guesses)
 summary(guesses[,-c(1,4)])
@@ -11,3 +14,44 @@ tapply(X = guesses$error, INDEX = guesses$race, FUN = var)
 boxplot(error ~ tru_age, data = guesses, main = "Boxplot of Error by True Age", xlab = "True Age", ylab = "Error")
 boxplot(error ~ sex, data = guesses, main = "Boxplot of Error by Sex", xlab = "Sex", ylab = "Error")
 boxplot(error ~ race, data = guesses, main = "Boxplot of Error by Race", xlab = "Race", ylab = "Error")
+
+#ggplot2 versions
+qplot(guesses$error,
+      geom="histogram",
+      binwidth = 5,  
+      main = "Histogram of Errors", 
+      xlab = "Error", 
+      ylab = "Frequency",
+      fill=I("blue"), 
+      col=I("red"), 
+      alpha=I(.2),
+      xlim=c(-30,20))
+
+qplot(guesses$error,
+      geom="histogram",
+      binwidth = 2,  
+      main = "Histogram of Absolute Errors", 
+      xlab = "Absolute Error", 
+      ylab = "Frequency",
+      fill=I("blue"), 
+      col=I("red"), 
+      alpha=I(.2),
+      xlim=c(0, 20))
+
+ggplot(guesses, aes(x=factor(tru_age), y=error, group=tru_age)) + 
+  geom_boxplot() +
+  scale_x_discrete("True Age", labels = guesses$tru_age, breaks = guesses$tru_age) +
+  ylab("Error") +
+  ggtitle("Boxplot of Error by True Age")
+
+ggplot(guesses, aes(x=factor(sex), y=error, group=sex)) + 
+  geom_boxplot() +
+  scale_x_discrete("Sex", labels = guesses$sex, breaks = guesses$sex) +
+  ylab("Error") +
+  ggtitle("Boxplot of Error by Sex")
+
+ggplot(guesses, aes(x=factor(race), y=error, group=race)) + 
+  geom_boxplot() +
+  scale_x_discrete("Race", labels = guesses$race, breaks = guesses$race) +
+  ylab("Error") +
+  ggtitle("Boxplot of Error by Race")
